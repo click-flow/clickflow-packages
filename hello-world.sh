2522e371e919f30a3d744ad9a8b7e9742be9addb
erik-ekberg-sg <erik@singlegrain.com>
2021-05-13 16:08:36 -0700

    Update hello-world.sh

--- hello-world.sh
+++ hello-world.sh
@@ -2,5 +2,5 @@
 
 ARG=${1-world}
 
-# * curl https://raw.githubusercontent.com/click-flow/clickflow-packages/main/hello-world.sh | sh
+# * curl https://raw.githubusercontent.com/click-flow/clickflow-packages/main/hello-world.sh | sh -s "Something else"
 echo Hello $ARG!
