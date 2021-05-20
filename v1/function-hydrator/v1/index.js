e7fe7ccec02398e84df80f404b31997f5499d927
erik-ekberg-sg <erik@singlegrain.com>
2021-05-20 15:19:55 -0700

    Scaffold option to read function maps straight from a json file

--- v1/function-hydrator/v1/index.js
+++ v1/function-hydrator/v1/index.js
@@ -1,15 +1,31 @@
+const fs = require('fs')
 const { invokeLambda } = require('./utils/invoke-lambda')
 const { v5: { createCloudeventStream } } = require('@1mill/cloudevents')
+
+let fileMaps
+if (process.env.FUNCTION_MAPS_FILE) {
+	try {
+		fileMaps = fs.readFileSync(process.env.FUNCTION_MAPS_FILE, 'utf8')
+	} catch (err) {
+		console.error(err)
+	}
+}
 
 // * For each single cloudevent we only want to invoke a function once.
 // * So we remove any duplicate function maps.
 const functionMaps = [
-	...new Set(
-		JSON.parse(
+	...new Set([
+		...JSON.parse(
+			fileMaps || JSON.stringify([])
+		).map(m => JSON.stringify(m)),
+		...JSON.parse(
 			process.env.FUNCTION_MAPS || JSON.stringify([])
-		).map(m => JSON.stringify(m))
-	)
+		).map(m => JSON.stringify(m)),
+	])
 ].map(m => JSON.parse(m))
+
+console.log(functionMaps)
+return
 
 // * For reach cloudevent type we only want to create one subscription
 // * per type. So this returns a list of unique cloudevent types we
